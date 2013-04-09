e15190127a219276859a4a347fb7bd6ad0356e00
Rob Scanlon <robscanlon@gmail.com>
2013-04-09 16:44:43 -0400

    make the threecol be better spaced

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -120,7 +120,7 @@ defaultLayouts = smartBorders(avoidStruts(
   -- and remaining windows tile on the right. By default each area
   -- takes up half the screen, but you can resize using "super-h" and
   -- "super-l".
-  ResizableTall 1 (3/100) (1/2) []
+  ResizableTall 1 (3/100) (3/4) []
 
   -- Mirrored variation of ResizableTall. In this layout, the large
   -- master window is at the top, and remaining windows tile at the
@@ -141,7 +141,7 @@ defaultLayouts = smartBorders(avoidStruts(
   -- the available space. Remaining windows tile to both the left and
   -- right of the master window. You can resize using "super-h" and
   -- "super-l".
-  ||| ThreeColMid 1 (3/100) (3/4)
+  ||| ThreeColMid 1 (3/100) (2/3)
 
   -- Circle layout places the master window in the center of the screen.
   -- Remaining windows appear in a circle around it
